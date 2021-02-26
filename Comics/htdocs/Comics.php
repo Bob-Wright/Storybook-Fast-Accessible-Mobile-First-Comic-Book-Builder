953ab17debe705d8fecf6822e1266ede402a6b9e
Bob Wright <35855133+Bob-Wright@users.noreply.github.com>
2021-02-26 14:33:48 -0500

    Add files via upload

---
+++ Comics/htdocs/Comics.php
@@ -0,0 +1,244 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Synthetic Reality Storybook</title>
+	<!-- <base href="./"> -->
+	<base href="https://syntheticreality.net/Comics/">
+	<base href="https://syntheticreality.net/Comics/Comics.php" rel="canonical">
+	<meta property="og:url" content="https://syntheticreality.net/Comics/Comics.php" >
+	<meta property="og:type" content="website" >
+	<meta property="og:title" content= "Synthetic Reality Storybook" >
+	<meta property="og:description" content="Comic Book Gallery by Bob Wright">
+	<meta property="og:image:type" content="image/png" >
+	<meta property="og:image:width" content="1800" >
+	<meta property="og:image:height" content="960" >
+	<meta property="og:image" content="https://syntheticreality.net/Comics/Landing.png" >
+	<meta property="fb:app_id" content="2917501244986193" >
+    <meta name="description" content="Synthetic Reality Storybook comic book gallery">
+    <meta name="author" content="Bob Wright and other contributors">
+    <!-- Bootstrap core CSS -->
+	<link href="./css/bootstrap.min.css" rel="stylesheet">
+	<!-- Material Design Bootstrap -->
+	<link rel="stylesheet" href="./css/mdb.min.css">
+	<link rel='stylesheet' href="./css/colorPalette.css">
+	<link rel='stylesheet' href="./css/textColorPalette.css">
+	<link rel='stylesheet' href="./css/LiteThemes.css">
+	<meta name="theme-color" content="#563d7c">
+<style>
+.pageWrapper:before {
+    content: ' ';
+    display: block;
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+/*	background: #008; */
+	z-index: -1;
+    opacity: 0.4;
+    background-image: url("../Images/ScreenShot224.jpg");
+	background-position: top center;
+    background-repeat: no-repeat;
+    -ms-background-size: 100% 100%;
+    -o-background-size: 100% 100%;
+    -moz-background-size: 100% 100%;
+    -webkit-background-size: 100% 100%;
+    background-size: 100% 100%;
+}
+      .bd-placeholder-img {
+        font-size: 1.125vw;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+@media (min-width: 1200px) {
+#headerContent, #navbarContent {
+ font-size: 1.4vw;
+}
+.card .card-body .card-text {
+ font-size: 1.4vw;
+}
+footer {
+ font-size: 1.4vw;
+}
+.navButton {
+ font-size: 1.2vw;
+}
+.galleryButton {
+ font-size: 1.2vw;
+}
+i {
+  font-size:1.4vw;
+}
+}
+footer {
+  padding-top: 3vw;
+  padding-bottom: 3vw;
+}
+
+footer p {
+  margin-bottom: .25vw;
+}
+</style>
+</head>
+
+<body>
+<!--Main Navigation-->
+<header id="anchor1">
+  <div class="collapse peach-gradient" id="navbarHeader">
+    <div class="container">
+      <div id="headerContent" class="row">
+        <div class="col-sm-8 col-md-7 py-4 indigo">
+          <h4 class="text-white"><b>About</b></h4>
+          <p class="text-white">This is a gallery of the Storybook Comic Books collection from Synthetic Reality. Click a card image to open the comic. There are also documents about the concepts behind this site and its contents.<br>We welcome comments and suggestions and good ideas.<br><br>
+		  <b>Learn to code so that when the Battle with the Robots begins you will be ready to fight back!</b><br><br>
+		  Portions &copy; 2021 Isoblock, USA, and by others.
+		  </p>
+        </div>
+		<div class="col-sm-4 offset-md-1 py-4 blue-grey lighten-2">
+		  <button class="btn btn-lg btn-light-blue accent1">
+			<a  class="text-dark navButton" href="./Copyrights.html" hreflang="en">Copyright Notices</a>
+		  </button>
+		<br>
+		  <button class="btn btn-lg btn-light-blue accent1">
+			<a  class="text-dark navButton" href="./Disclaimer.html" hreflang="en">Works of Fiction Disclaimer</a>
+		  </button>
+		<br>
+		  <button class="btn btn-lg btn-light-blue accent1">
+			<a  class="text-dark navButton" href="./Synopsis.html" hreflang="en">About Storybook</a>
+		  </button>
+		<br>
+		  <button class="btn btn-lg btn-light-green accent1">
+			<a  class="text-dark navButton" href="../Storybook/OauthPortal.php" hreflang="en">Run the Storybook Comic Builder</a>
+		  </button>
+		<br>
+		  <button class="btn btn-lg btn-blue accent1">
+			<a class="text-white navButton" href="https://www.facebook.com/Storybook-Comic-Book-Builder-112113734045065">Storybook on Facebook</a>
+		  </button>
+		<br>
+		  <button class="btn btn-lg btn-orange accent1">
+			<a class="text-white navButton" href="https://github.com/Bob-Wright/Facebook-Comic-Book-Maker-Application-on-AWS" hreflang="en">Storybook on Github</a>
+		  </button>
+		<br>
+		  <button class="btn btn-lg btn-yellow accent1">
+			<span class="mdi mdi-email"></span>&nbsp;<a class="text-dark navButton" href="mailto:syntheticreality.storybook@gmail.com?subject=Storybook Comics" hreflang="en">Send us an EMail</a>
+		  </button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <nav class="navbar navbar-dark bg-dark purple-gradient shadow-sm">
+    <div class="container d-flex justify-content-between indigo">
+      <a id="navbarContent" href="#" class="navbar-brand d-flex align-items-center" title="Comics logo">
+<svg version="1.0" xmlns="http://www.w3.org/2000/svg" class="bi-layout-wtf"
+		 width="60.000000pt" height="42.000000pt" viewBox="0 0 60.000000 42.000000">
+		<g transform="translate(0.000000,42.000000) scale(0.100000,-0.100000)"
+		fill="white" stroke="white" stroke-width="5">
+		<path d="M467 413 c-2 -2 -32 -4 -67 -4 -53 -1 -67 -5 -87 -26 l-25 -24 -43
+		21 c-36 17 -58 20 -136 18 l-93 -3 3 -150 c1 -82 6 -153 10 -158 4 -4 24 -1
+		44 8 41 17 85 19 107 5 11 -7 6 -10 -22 -10 -40 0 -121 -31 -133 -51 -9 -13
+		-1 -12 75 16 51 19 97 14 149 -16 l34 -20 22 27 c20 25 25 26 92 22 48 -2 78
+		-9 90 -20 22 -20 73 -34 73 -20 0 5 -24 21 -53 36 -43 21 -69 26 -127 26 -65
+		0 -71 2 -59 16 13 16 35 16 197 -2 l52 -6 0 79 c0 43 5 109 10 147 6 38 7 73
+		3 77 -9 7 -110 18 -116 12z m88 -70 c-4 -26 -9 -86 -11 -133 l-3 -85 -28 1
+		c-15 1 -59 7 -97 14 -64 11 -70 11 -97 -9 -26 -20 -29 -20 -38 -3 -9 15 -10
+		15 -11 -5 0 -27 2 -27 -61 -7 -48 16 -112 15 -152 -1 -16 -7 -17 3 -17 129 l0
+		136 69 0 c70 0 149 -21 163 -43 4 -7 8 -40 8 -74 0 -35 4 -63 10 -63 6 0 10
+		30 10 69 0 52 5 74 19 92 17 23 26 24 131 27 l112 3 -7 -48z"/>
+		</g>
+		</svg>
+        <b>&emsp;Synthetic Reality Storybook</b></a>
+	  <button class="navbar-toggler" title="Toggle navigation" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+		<svg version="1.0" xmlns="http://www.w3.org/2000/svg"
+		 width="42.000000pt" height="30.000000pt" viewBox="0 0 42.000000 30.000000"
+		 preserveAspectRatio="xMidYMid meet">
+		<g transform="translate(0.000000,30.000000) scale(0.100000,-0.100000)"
+		fill="white" stroke="white">
+		<path d="M0 270 l0 -30 210 0 210 0 0 30 0 30 -210 0 -210 0 0 -30z"/>
+		<path d="M0 150 l0 -30 210 0 210 0 0 30 0 30 -210 0 -210 0 0 -30z"/>
+		<path d="M0 35 l0 -35 210 0 210 0 0 35 0 35 -210 0 -210 0 0 -35z"/>
+		</g>
+		</svg>
+	  </button>
+    </div>
+  </nav>
+</header>
+
+
+<main role="main">
+    <section class="container-fluid text-center #929fba mdb-color lighten-3">
+      <h1 class="text-dark font-weight-bolder">Comic Books Gallery</h1>
+      <p class="lead text-dark font-weight-bolder">This is the Synthetic Reality Storybook comic books gallery.<br>Each card represents a comic book or document. Click the cover image to view the item.</p>
+    </section>
+
+  <section class="album py-5">
+     <img class="sr-only" alt="The page has a fractal as a background image." width="1px" height="1px" src="./Img/1x1pixel.png">
+
+    <div class="container">
+    <div class="row pageWrapper">
+<?php
+// list of comics as gallery card entries
+$comicsBase = '/home/bitnami/Comics/htdocs/';
+if ($handle = opendir($comicsBase)) {
+    //echo "Directory handle: $handle\n";
+    //echo "Entries:\n";
+    /* loop over the directory. */
+	while (false !== ($entry = readdir($handle))) {
+		if(!(is_dir($comicsBase.$entry))) {
+			$filenameArray = explode('.', $entry);
+			// check the filetype/extension for CARD files
+			if (preg_match('/card/i', $filenameArray[1])) {
+				include($comicsBase.$entry);
+			}
+		}
+	}
+closedir($handle);
+}
+?>
+      </div>
+    </div>
+  </section>
+
+</main>
+
+<footer class="purple-gradient col-md-12 d-flex flex-direction:column justify-content-between align-items-center">
+    <p class="text-dark font-weight-bolder col-8">This Synthetic Reality Storybook Comic Books Gallery is &copy; IsoBlock.com, but you are free to download and customize it for yourself!<br>
+    <a href="https://syntheticreality.net/">
+		<svg xmlns="http://www.w3.org/2000/svg" title="IsoBlock logo" width="40" height="40" fill="black" stroke="black" stroke-width="2" aria-hidden="false" class="mr-2" viewBox="0 0 40 40" focusable="false"><g transform="translate(0.000000,180.000000) scale(0.100000,-0.100000)">
+		<title>IsoBlock logo</title> 
+		<path class="st0" d="M1.6,1696.71l-1.3-101.39l9.78-34.35c11.47-40.7,22.73-80.74,33.78-119.9c4.46-15.75,8.95-29.76,10.26-31.09
+			c1.53-2.2,38.83-3.29,172.75-4.88l170.78-2.02l1.33,103.78l1.33,103.78l-11.7,40.7c-6.37,22.54-17.85,63.01-25.5,89.93
+			c-7.65,26.69-14.47,49.67-15.12,50.77c-0.65,1.1-73.71,2.83-173.2,4.01L2.9,1798.1L1.6,1696.71z M235.16,1687.63l27.48-91.69
+			l-0.93-20.92l-0.93-20.92l-21.47,75.49l-21.69,75.49l-39.53,0.47l-39.75,0.47l-1.29-48.61l-1.06-48.4l-61.73,1.17l-61.51,1.38
+			l0.4,82.86c0.36,45.57,1.08,84.16,1.76,85.68c0.69,2.17,21.35,2.36,96.86,1.04l95.93-1.79L235.16,1687.63z M344.82,1752.19
+			c4.25-14.88,14.87-52.73,23.8-84.24c8.92-31.73,16.13-58.41,16.12-59.72s-19.79-1.95-54.88-1.53l-55.08,0.65l-14.7,48.8
+			c-16.4,54.93-35.15,117.3-36.43,121.46c-0.64,1.97,11.58,2.48,56.44,1.95l57.3-0.68L344.82,1752.19z M208.06,1687.95
+			c-0.02-1.53,11.02-41.34,24.41-88.39c13.39-47.04,24.43-86.64,24.42-87.95c-0.02-1.53-8.91-2.08-26.01-1.44l-25.98,0.96
+			l-11.27,39.39c-18.92,66.3-38.88,138.28-38.86,139.59c0.01,0.87,12.01,1.38,26.67,1.21
+			C202.33,1691.07,208.09,1690.35,208.06,1687.95z M155.8,1630.56c8.24-33.24,36.98-130.19,39.17-133.05
+			c1.75-1.98,11.74-2.76,40.39-2.66l37.99,0.21l1.29,48.61l1.06,48.4l56.63-0.67l56.63-0.67l-1.09-85.03l-1.09-85.03l-93.05,1.1
+			l-93.05,1.1l-4.25,14.88c-2.34,8.1-9.98,35.88-17.42,61.7c-7.22,25.82-16.78,59.95-21.46,75.92c-7.23,25.38-8.26,31.5-8.01,51.12
+			c0.16,12.21,0.72,21.8,1.16,21.36C151.13,1647.41,153.47,1639.53,155.8,1630.56z M143.28,1572.49c3.4-11.82,8.29-29.32,10.83-38.94
+			c2.54-9.62,9.76-35.66,16.36-57.98c6.38-22.32,12.11-43.1,12.95-46.6l1.26-5.9l-60.63,0.72l-60.63,0.72l-4.45,17.06
+			c-5.29,20.78-40.56,145.71-42.28,150.31c-1.07,2.85,6.26,3.2,59.56,2.57l60.85-0.72L143.28,1572.49z"/>
+	</g>
+	</svg><b>Visit the Synthetic Reality homepage.</b></a></p>
+	<div class="float-right">
+    <!-- <button class="btn btn-pink">
+      <a href="../Storybook/OauthPortal.php" class="text-dark">Comic Book Builder</a>
+    </button> -->
+    <button class="btn btn-yellow">
+      <a href="./Comics.php#anchor1" class="text-dark navButton">Back to top</a>
+    </button>
+	</div>
+</footer>
+<script src="./js/jquery.min.js"></script>
+<script src= "./js/bootstrap.min.js"></script>
+
+</body>
+</html>
